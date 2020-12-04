d2c54d5988f5ce428e54aed69834c3433add7542
Seventeenth <sev17th@gmail.com>
2020-12-04 23:52:12 +0300

    added files

---
+++ MyVK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyVK
+//
+//  Created by Валерий on 03.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var loginInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    @IBAction func loginButtomPressed(_ sender: Any) {
+    }
+}
+
+
